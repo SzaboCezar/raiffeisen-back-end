1475567722c8f2470245afd698fff87e019b88ac
SzaboCezar <szabocezar@yahoo.com>
2024-02-19 11:23:29 +0200

    Created the gradle project for Spring Boot

---
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/RaiffeisenBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ubb.raiffeisen.raiffeisenbackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiffeisenBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiffeisenBackendProjectApplication.class, args);
+	}
+
+}
---
+++ raiffeisen-backend-project/src/test/java/com/ubb/raiffeisen/raiffeisenbackendproject/RaiffeisenBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubb.raiffeisen.raiffeisenbackendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiffeisenBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
