a382df1fed523667f9f81137da96d70bb3e9f54d
SzaboCezar <szabocezar@yahoo.com>
2024-02-22 15:44:50 +0200

    Created documentation for the CardController

--- raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/CreditCard/CreditCardController.java
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/CreditCard/CreditCardController.java
@@ -7,23 +7,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Controller for managing credit card-related operations.
+ */
 @RestController
 public class CreditCardController {
-
     private CreditCardJpaRepository cardJpaRepository;
     private UserJpaRepository userJpaRepository;
 
+    /**
+     * Constructs a new CreditCardController with the specified repositories.
+     *
+     * @param cardJpaRepository The repository for credit card entities.
+     * @param userJpaRepository The repository for user entities.
+     */
     public CreditCardController(CreditCardJpaRepository cardJpaRepository, UserJpaRepository userJpaRepository) {
         this.cardJpaRepository = cardJpaRepository;
         this.userJpaRepository = userJpaRepository;
     }
 
-
+    /**
+     * Retrieves a list of all credit cards.
+     *
+     * @return A list of all credit cards.
+     */
     @GetMapping(path = "/credit-cards")
     public List<CreditCard> getCreditCardList(){
            return cardJpaRepository.findAll();
     }
 
+    /**
+     * Retrieves the credit card with the specified ID.
+     *
+     * @param cardId The ID of the credit card to retrieve.
+     * @return The credit card with the specified ID, if found.
+     * @throws CardNotFoundException If no credit card with the specified ID exists.
+     */
     @GetMapping(path = "/credit-card/{cardId}")
     public Optional<CreditCard> findById(@PathVariable Long cardId){
         Optional<CreditCard> creditCard = cardJpaRepository.findById(cardId);
@@ -31,7 +50,14 @@ public class CreditCardController {
         return creditCard;
     }
 
-
+    /**
+     * Creates a new credit card for the specified user.
+     *
+     * @param user_id The ID of the user for whom to create the credit card.
+     * @param creditCard The credit card to create.
+     * @return The created credit card.
+     * @throws UserNotFoundException If no user with the specified ID exists.
+     */
     @PostMapping(path = "/user/{user_id}/credit-card")
     private CreditCard createCreditCardForUser(@PathVariable Long user_id, @RequestBody CreditCard creditCard){
         Optional<User> findUser = userJpaRepository.findById(user_id);
@@ -44,6 +70,12 @@ public class CreditCardController {
         return cardJpaRepository.save(creditCard);
     }
 
+
+    /**
+     * Deletes the credit card with the specified ID.
+     *
+     * @param id The ID of the credit card to delete.
+     */
     @PostMapping(path = "/credit-card/delete{id}")
     public void deleteById(@PathVariable Long id){
         cardJpaRepository.deleteById(id);
