389c77b07b7ad939dc42b46c36181b3d1d3ba32a
SzaboCezar <szabocezar@yahoo.com>
2024-02-22 13:30:47 +0200

    Commented services because they are no longer in use

--- raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/CreditCard/CreditCardService.java
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/CreditCard/CreditCardService.java
@@ -1,64 +1,64 @@
-package com.ubb.raiffeisen.raiffeisenbackendproject.CreditCard;
-
-import com.ubb.raiffeisen.raiffeisenbackendproject.User.User;
-import com.ubb.raiffeisen.raiffeisenbackendproject.User.UserService;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.function.Predicate;
-
-@Service
-public class CreditCardService {
-
-    private List<CreditCard> creditCardList = new ArrayList<>();
-
-    public UserService userService = new UserService();
-
-    User user1 = userService.findById(1L);
-    User user2 = userService.findById(2L);
-    User user3 = userService.findById(3L);
-    User user4 = userService.findById(4L);
-    User user5 = userService.findById(5L);
-    private static Long counter = 6L;
-
-    {
-        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2025, 12, 31), "123", 1000L, 0, user1));
-        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2026, 10, 15), "456", 2000L, 100, user2));
-        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2024, 8, 28), "789", 1500L, 50, user3));
-        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2023, 5, 20), "321", 3000L, 200, user4));
-        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2025, 3, 10), "654", 2500L, 150, user5));
-    }
-
-    private static String generateRandomCardNumber() {
-        Random random = new Random();
-        StringBuilder cardNumberBuilder = new StringBuilder();
-        for (int i = 0; i < 16; i++) {
-            int digit = random.nextInt(10);
-            cardNumberBuilder.append(digit);
-        }
-        return cardNumberBuilder.toString();
-    }
-
-    public List<CreditCard> getCreditCardList(){
-        return creditCardList;
-    }
-
-    public CreditCard findById(Long id){
-        Predicate<? super CreditCard> predicate = creditId -> creditId.getId().equals(id);
-        return creditCardList.stream().filter(predicate).findFirst().orElse(null);
-    }
-
-    public void saveCreditCard(CreditCard creditCard){
-        creditCard.setId(counter++);
-        creditCardList.add(creditCard);
-    }
-
-    public void deleteById(Long id){
-        Predicate<? super CreditCard> predicate = creditId -> creditId.getId().equals(id);
-        creditCardList.removeIf(predicate);
-    }
-
-}
+//package com.ubb.raiffeisen.raiffeisenbackendproject.CreditCard;
+//
+//import com.ubb.raiffeisen.raiffeisenbackendproject.User.User;
+//import com.ubb.raiffeisen.raiffeisenbackendproject.User.UserService;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Random;
+//import java.util.function.Predicate;
+//
+//@Service
+//public class CreditCardService {
+//
+//    private List<CreditCard> creditCardList = new ArrayList<>();
+//
+//    public UserService userService = new UserService();
+//
+//    User user1 = userService.findById(1L);
+//    User user2 = userService.findById(2L);
+//    User user3 = userService.findById(3L);
+//    User user4 = userService.findById(4L);
+//    User user5 = userService.findById(5L);
+//    private static Long counter = 6L;
+//
+//    {
+//        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2025, 12, 31), "123", 1000L, 0, user1));
+//        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2026, 10, 15), "456", 2000L, 100, user2));
+//        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2024, 8, 28), "789", 1500L, 50, user3));
+//        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2023, 5, 20), "321", 3000L, 200, user4));
+//        creditCardList.add(new CreditCard(counter++, generateRandomCardNumber(), LocalDate.of(2025, 3, 10), "654", 2500L, 150, user5));
+//    }
+//
+//    private static String generateRandomCardNumber() {
+//        Random random = new Random();
+//        StringBuilder cardNumberBuilder = new StringBuilder();
+//        for (int i = 0; i < 16; i++) {
+//            int digit = random.nextInt(10);
+//            cardNumberBuilder.append(digit);
+//        }
+//        return cardNumberBuilder.toString();
+//    }
+//
+//    public List<CreditCard> getCreditCardList(){
+//        return creditCardList;
+//    }
+//
+//    public CreditCard findById(Long id){
+//        Predicate<? super CreditCard> predicate = creditId -> creditId.getId().equals(id);
+//        return creditCardList.stream().filter(predicate).findFirst().orElse(null);
+//    }
+//
+//    public void saveCreditCard(CreditCard creditCard){
+//        creditCard.setId(counter++);
+//        creditCardList.add(creditCard);
+//    }
+//
+//    public void deleteById(Long id){
+//        Predicate<? super CreditCard> predicate = creditId -> creditId.getId().equals(id);
+//        creditCardList.removeIf(predicate);
+//    }
+//
+//}
--- raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/User/UserService.java
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/User/UserService.java
@@ -1,44 +1,44 @@
-package com.ubb.raiffeisen.raiffeisenbackendproject.User;
-
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Predicate;
-
-//@Service
-public class UserService {
-
-    private List<User> userList = new ArrayList<>();
-    private Long counter = 1L;
-
-     {
-         userList.add(new User(counter++, "John", "Doe", "john.doe@example.com", "password123", LocalDate.of(1998, 5, 15), "123 Main St"));
-         userList.add(new User(counter++, "Alice", "Smith", "alice.smith@example.com", "password456", LocalDate.of(1999, 8, 25), "456 Elm St"));
-         userList.add(new User(counter++, "Bob", "Johnson", "bob.johnson@example.com", "password789", LocalDate.of(2000, 2, 10), "789 Oak St"));
-         userList.add(new User(counter++, "Emily", "Brown", "emily.brown@example.com", "passwordabc", LocalDate.of(1997, 11, 30), "101 Pine St"));
-         userList.add(new User(counter++, "Michael", "Davis", "michael.davis@example.com", "passwordxyz", LocalDate.of(1996, 7, 8), "202 Cedar St"));
-     }
-
-     public List<User> getListOfUsers(){
-         return userList;
-     }
-
-     public User findById(Long id){
-         Predicate<? super User> predicate = user -> Objects.equals(user.getId(), id);
-         return userList.stream().filter(predicate).findFirst().orElse(null);
-     }
-
-     public void saveUser(User user){
-         user.setId(counter++);
-         userList.add(user);
-     }
-
-     public void deleteUserById(Long id){
-         Predicate<? super User> predicate = user -> user.getId().equals(id);
-         userList.removeIf(predicate);
-     }
-
-}
+//package com.ubb.raiffeisen.raiffeisenbackendproject.User;
+//
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Objects;
+//import java.util.function.Predicate;
+//
+////@Service
+//public class UserService {
+//
+//    private List<User> userList = new ArrayList<>();
+//    private Long counter = 1L;
+//
+//     {
+//         userList.add(new User(counter++, "John", "Doe", "john.doe@example.com", "password123", LocalDate.of(1998, 5, 15), "123 Main St"));
+//         userList.add(new User(counter++, "Alice", "Smith", "alice.smith@example.com", "password456", LocalDate.of(1999, 8, 25), "456 Elm St"));
+//         userList.add(new User(counter++, "Bob", "Johnson", "bob.johnson@example.com", "password789", LocalDate.of(2000, 2, 10), "789 Oak St"));
+//         userList.add(new User(counter++, "Emily", "Brown", "emily.brown@example.com", "passwordabc", LocalDate.of(1997, 11, 30), "101 Pine St"));
+//         userList.add(new User(counter++, "Michael", "Davis", "michael.davis@example.com", "passwordxyz", LocalDate.of(1996, 7, 8), "202 Cedar St"));
+//     }
+//
+//     public List<User> getListOfUsers(){
+//         return userList;
+//     }
+//
+//     public User findById(Long id){
+//         Predicate<? super User> predicate = user -> Objects.equals(user.getId(), id);
+//         return userList.stream().filter(predicate).findFirst().orElse(null);
+//     }
+//
+//     public void saveUser(User user){
+//         user.setId(counter++);
+//         userList.add(user);
+//     }
+//
+//     public void deleteUserById(Long id){
+//         Predicate<? super User> predicate = user -> user.getId().equals(id);
+//         userList.removeIf(predicate);
+//     }
+//
+//}
