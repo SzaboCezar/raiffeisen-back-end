c6f0aa096a2520b5ffc1f2eabca57bd165c5206c
SzaboCezar <szabocezar@yahoo.com>
2024-02-23 20:05:08 +0200

    Getting Started with Spring Security

--- raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/User/User.java
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/User/User.java
@@ -3,9 +3,16 @@ package com.ubb.raiffeisen.raiffeisenbackendproject.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ubb.raiffeisen.raiffeisenbackendproject.CreditCard.CreditCard;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "user_details")
 public class User {
     @Id
@@ -20,96 +27,4 @@ public class User {
     @OneToOne(cascade = CascadeType.ALL)
     @JsonIgnore
     private CreditCard creditCard;
-
-    public User(Long userID, String firstName, String lastName, String email, String password, LocalDate dateOfBirth, String address) {
-        this.userID = userID;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-    }
-
-    public User() {
-
-    }
-
-    public Long getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Long userID) {
-        this.userID = userID;
-    }
-
-    public CreditCard getCreditCard() {
-        return creditCard;
-    }
-
-    public void setCreditCard(CreditCard creditCard) {
-        this.creditCard = creditCard;
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String adress) {
-        this.address = adress;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userID=" + userID +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", dateOfBirth=" + dateOfBirth +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
