faea8b2c771e4e0f386eb515b6f6ed7a05855367
SzaboCezar <szabocezar@yahoo.com>
2024-02-29 12:33:20 +0200

    Handle exception (CreditCardNotFound) for the CreditCard class

--- raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/HandleException/CustomExceptionHandler.java
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/HandleException/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ubb.raiffeisen.raiffeisenbackendproject.HandleException;
 
+import com.ubb.raiffeisen.raiffeisenbackendproject.CreditCard.CardNotFoundException;
 import com.ubb.raiffeisen.raiffeisenbackendproject.User.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,12 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex) {
+        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(CardNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleCreditCardNotFoundException(Exception ex){
         ErrorDetails errorDetails = new ErrorDetails(ex.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
