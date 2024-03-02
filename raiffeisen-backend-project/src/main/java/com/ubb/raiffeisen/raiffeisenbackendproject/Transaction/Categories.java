f2f7c6891db0a6c83ddefe760859585ece4d422b
SzaboCezar <szabocezar@yahoo.com>
2024-03-02 22:15:09 +0200

    Change string to type enum for categories

---
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/Transaction/Categories.java
@@ -0,0 +1,8 @@
+package com.ubb.raiffeisen.raiffeisenbackendproject.Transaction;
+
+public enum Categories {
+    GROCERIES,
+    MALL,
+    CINEMA,
+    ECO
+}
--- raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/Transaction/Transaction.java
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/Transaction/Transaction.java
@@ -2,10 +2,7 @@ package com.ubb.raiffeisen.raiffeisenbackendproject.Transaction;
 
 import com.ubb.raiffeisen.raiffeisenbackendproject.CreditCard.CreditCard;
 import com.ubb.raiffeisen.raiffeisenbackendproject.User.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,7 +19,9 @@ public class Transaction {
     @Id
     @GeneratedValue
     private Long transactionID;
-    private String type;
+
+    @Enumerated(EnumType.STRING)
+    private Categories categories;
     private double amount;
     private LocalDate date;
     private String description;
