80db58a6a29c1dbbe893c565ac010c89ee48dedd
SzaboCezar <szabocezar@yahoo.com>
2024-03-13 09:56:54 +0200

    Refactor credit card entity (Added Lombok)

--- raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/CreditCard/CreditCard.java
+++ raiffeisen-backend-project/src/main/java/com/ubb/raiffeisen/raiffeisenbackendproject/CreditCard/CreditCard.java
@@ -2,9 +2,16 @@ package com.ubb.raiffeisen.raiffeisenbackendproject.CreditCard;
 
 import com.ubb.raiffeisen.raiffeisenbackendproject.User.User;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "credit_cards")
 public class CreditCard {
     @Id
@@ -17,85 +24,4 @@ public class CreditCard {
     private Integer points;
     @OneToOne
     private User user;
-
-    public CreditCard(Long creditCardID, String cardNumber, LocalDate expirationDate, String CVV, Long amount, Integer points) {
-        this.creditCardID = creditCardID;
-        this.cardNumber = cardNumber;
-        this.expirationDate = expirationDate;
-        this.CVV = CVV;
-        this.amount = amount;
-        this.points = points;
-    }
-
-    public CreditCard() {
-
-    }
-
-    public Long getCreditCardID() {
-        return creditCardID;
-    }
-
-    public void setCreditCardID(Long creditCardID) {
-        this.creditCardID = creditCardID;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    public String getCVV() {
-        return CVV;
-    }
-
-    public void setCVV(String CVV) {
-        this.CVV = CVV;
-    }
-
-    public Long getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Long amount) {
-        this.amount = amount;
-    }
-
-    public Integer getPoints() {
-        return points;
-    }
-
-    public void setPoints(Integer points) {
-        this.points = points;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "CreditCard{" +
-                "creditCardID=" + creditCardID +
-                ", cardNumber='" + cardNumber + '\'' +
-                ", expirationDate=" + expirationDate +
-                ", CVV='" + CVV + '\'' +
-                ", amount=" + amount +
-                ", points=" + points +
-                '}';
-    }
 }
